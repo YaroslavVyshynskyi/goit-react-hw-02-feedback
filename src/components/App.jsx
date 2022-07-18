bd320f2cf3082e9b3afd3b8f5ce5e6afea58330a
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-07-18 21:11:40 +0300

    add styles to section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,22 +3,6 @@ import Section from "./Section/Section";
 import FeedBackOptions from "./FeedbackOptions/FeedbackOptions";
 import Statistics from "./Statistics/Statistics";
 import { Component } from "react";
-// state = {
-//   good: 0,
-//   neutral: 0,
-//   bad: 0
-// };
-
-// export const App = () => {
-//   return (
-//     <div>
-//       <Section initialValue={10} title="Please leave feedback">
-//       <FeedbackOptions options={ good, neutral, total} onLeaveFeedback={handleGoodFb, handleNeutralFb, handleBadFb} />
-//       <Statistics good={0} neutral={0} bad={0} total={0} positivePercentage={0}></Statistics>
-//       </Section>
-//     </div>
-//   );
-// };
 
 class App extends Component { 
   state = {
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import css from "./Section.module.css";
 
-const Section = ({ title, children }) => { 
+const Section = ({ title, children }) => {
     return (
-        <>
+        <section className={css.counter__section}>
             <p className={css.section_title}>{title}</p>
             {children}
-        </>    
+        </section>    
     );    
 }
 
